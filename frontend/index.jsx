e754633fb2dfc28d0ab9bd96567e6d9e0402887e
Kevin Choy <kevinchoy22@gmail.com>
2018-08-10 12:24:20 -0700

    push to heroku 6

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -22,7 +22,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // TESTING END
 
     const root = document.getElementById('root');
-    ReactDOM.render(<h1>This is also working</h1>);
+    ReactDOM.render(<h1>This is also working</h1>, root);
     // ReactDOM.render(<Root store = {store} />, root);
 });
 
