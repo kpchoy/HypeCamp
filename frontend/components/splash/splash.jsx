edc8cc30ac111cdacd9e1736f080c7ad594fc769
Kevin Choy <kevinchoy22@gmail.com>
2018-08-12 21:45:49 -0700

    begin active storage for pics

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -6,11 +6,16 @@ class Splash extends React.Component {
   componentDidMount() {
     this.props.fetchCampsites();
   }
-  
+
   render() {
+    console.log(this.props.campsites[0]);
     return (
       <div>
         <h1>this is splash</h1>
+        <section>
+          Main picture  
+        </section>
+
         {this.props.campsites.map((campsite) => (
           <p>{campsite.title}</p>
         ))}
