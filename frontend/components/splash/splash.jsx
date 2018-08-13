a147a72d37fd302ce38f711f5b5b846aecef86dd
Kevin Choy <kevinchoy22@gmail.com>
2018-08-13 15:54:11 -0700

    added row of 3 to splash

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -27,13 +27,67 @@ class Splash extends React.Component {
               />
             </div>
 
+            <ul className="search-links-holder">
+              <li className="li-c-i-o"><a className="notextdec" href="">Check-In</a></li>
+              <li className="li-c-i-o"><a className="notextdec" href="">Check-Out</a></li>
+              <li className="splash-search-button">
+                <i className="fa fa-search" aria-hidden="true"></i>
+                <a className="notextdec" href=""> Search</a>
+              </li>
+            </ul>
+
           </div>
           <div className="uppersplash-r">
             <img src="https://s3-us-west-1.amazonaws.com/hypecamp-dev/main_photo.jpg" className="main-img"/>
           </div>
         </section>
 
-        <h1>this is splash</h1>
+        <section className="splash-bot">
+          <h1 className="discover-camp">Discover camping...</h1>
+          <ul className="camp-row">
+            <li>
+              <a href="">
+                <img src="https://s3-us-west-1.amazonaws.com/hypecamp-dev/01-splah-soto.webp" />
+                <p>Tent Camping</p>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <img src="https://s3-us-west-1.amazonaws.com/hypecamp-dev/02-splash-wtto.webp" />
+                <p>Ocean Views</p>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <img src="https://s3-us-west-1.amazonaws.com/hypecamp-dev/03-splash-gbm.webp" />
+                <p>Glamping</p>
+              </a>
+            </li>
+          </ul>
+
+          
+          <ul className="camp-row">
+            <li>
+              <a href="">
+                <img src="https://s3-us-west-1.amazonaws.com/hypecamp-dev/01-splah-soto.webp" />
+                <p>Tent Camping</p>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <img src="https://s3-us-west-1.amazonaws.com/hypecamp-dev/02-splash-wtto.webp" />
+                <p>Ocean Views</p>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <img src="https://s3-us-west-1.amazonaws.com/hypecamp-dev/03-splash-gbm.webp" />
+                <p>Glamping</p>
+              </a>
+            </li>
+          </ul>
+
+        </section>
        
        
 
