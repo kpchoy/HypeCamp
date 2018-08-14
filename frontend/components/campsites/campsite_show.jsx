d1ed3120eb23f14f9b374664d4b37331df2daf20
Kevin Choy <kevinchoy22@gmail.com>
2018-08-14 12:31:17 -0700

    styling show page

--- frontend/components/campsites/campsite_show.jsx
+++ frontend/components/campsites/campsite_show.jsx
@@ -6,7 +6,7 @@ class CampsiteShow extends React.Component {
     this.props.fetchCampsite(this.props.match.params.campsiteId);
   }  
 
-
+ 
 
   render() {
     const { campsite, campsitePhotos } = this.props;
@@ -16,10 +16,41 @@ class CampsiteShow extends React.Component {
     }
 
     return (
-      <div>
-        <h1>hello this is campsite show</h1>
-        <p>{campsite.title}</p>
+      <div className="main-camp-show">
+        <ul className="upper-images">
+          <li><img src={campsitePhotos[0].image_url} /></li>
+          <li><img src={campsitePhotos[1].image_url} /></li>
+          <li><img src={campsitePhotos[3].image_url} /></li>
+        </ul>
         
+        <section className="show-body">
+          <div className="show-body-left">
+            <h1>{campsite.title}</h1>
+          </div>
+          <div className="show-body-right">
+            <div className="show-body-price">
+              <h1>$ {campsite.daily_rate}</h1>
+              <p>per night</p>
+            </div>
+            
+            <ul>
+              <li>
+                <h3>Check In</h3>
+                
+              </li>
+              <li>
+                <h3>Check Out</h3>
+                
+              </li>
+              <li>
+                <h3>Guests</h3>
+              </li>
+            </ul>
+            <section>
+              <a href="">Request to Book</a>
+            </section>
+          </div>
+        </section>
       </div>
     );
 
